c77fed36397414089691a9ca756b695fb5ec3587
isaac wambiri <isaacwambiri254@gmail.com>
2024-11-18 15:14:02 +0300

    updated cors

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,26 +1,4 @@
-const cors = require('cors');
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    // Allow any origin in development mode
-    if (!origin || process.env.NODE_ENV === 'development') {
-      return callback(null, true);
-    }
-
-    // Whitelist specific production origins
-    const allowedOrigins = [
-      'https://zuri-nine-rouge.vercel.app',
-      'http://localhost:3000',
-    ];
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true, // Allow cookies
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
